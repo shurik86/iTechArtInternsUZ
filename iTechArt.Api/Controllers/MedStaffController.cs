ee1750a9af688f1b828037718f284edcb39c870b
Ozodbek00 <kamoliddinovozodbek@mail.ru>
2022-11-12 23:28:26 +0500

    MedStaff Pagination ready

--- iTechArt.Api/Controllers/MedStaffController.cs
+++ iTechArt.Api/Controllers/MedStaffController.cs
@@ -103,9 +103,9 @@ namespace iTechArt.Api.Controllers
         /// Gets all data from database.
         /// </summary>
         [HttpGet("get_all")]
-        public async Task<ActionResult<IMedStaff[]>> ExportAsync([FromServices] IMedStaffService _medStaffService)
+        public async Task<ActionResult<IMedStaff[]>> ExportAsync([FromQuery] int pageIndex)
         {
-            return Ok(await _medStaffService.ExportMedStaffFileAsync());
+            return Ok(await _medStaffService.ExportMedStaffFileAsync(pageIndex));
         }
     }
 }
---
+++ iTechArt.Repository/PaginationHelpers/Pagination.cs
@@ -0,0 +1,12 @@
+﻿namespace iTechArt.Repository.PaginationHelpers
+{
+    internal static class Pagination
+    {
+        public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageIndex)
+        {
+            return PaginationConstant.PageSize > 0 && pageIndex > 0
+                ? source.Skip((pageIndex - 1) * PaginationConstant.PageSize).Take(PaginationConstant.PageSize)
+                : source.Take(PaginationConstant.PageSize);
+        }
+    }
+}
