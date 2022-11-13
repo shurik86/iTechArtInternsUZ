a2b610f96afaca0050d3b209fd8c613c21fce739
Khasan <k.kholmatov@student.inha.uz>
2022-11-13 22:48:30 +0500

    Export Xml file created for all entities

--- iTechArt.Api/Controllers/GroceryController.cs
+++ iTechArt.Api/Controllers/GroceryController.cs
@@ -19,21 +19,21 @@ namespace iTechArt.Api.Controllers
         }
 
         [HttpPost(ApiConstants.IMPORT),Obsolete]
-        public async ValueTask<IActionResult> ImportGroceryFiles(IFormFile file)
+        public async ValueTask<IActionResult> ImportGroceryFilesAsync(IFormFile file)
         {
             if (file != null && file.ContentType.Contains(CSV))
             {
-                await _groceryService.ImportCSVGrocery(file);
+                await _groceryService.ImportCSVGroceryAsync(file);
                 return Ok();
             }
             else if (file != null && file.ContentType.Contains(EXCEL)) 
             {
-                await _groceryService.ImportExcelGrocery(file);
+                await _groceryService.ImportExcelGroceryAsync(file);
                 return Ok();
             }
             else if (file != null && file.ContentType.Contains(XML))
             {
-                await _groceryService.ImportXMLGrocery(file);
+                await _groceryService.ImportXMLGroceryAsync(file);
                 return Ok();
             }
             return BadRequest();
@@ -43,11 +43,11 @@ namespace iTechArt.Api.Controllers
         /// will allow to upload the data from CSV file to db 
         /// </summary>
         [HttpPost(ApiConstants.IMPORTCSV)]
-        public async ValueTask<IActionResult> ImportCsvGroceryFile(IFormFile file)
+        public async ValueTask<IActionResult> ImportCsvGroceryFileAsync(IFormFile file)
         {
             if (file != null && file.ContentType.Contains(CSV))
             {
-                await _groceryService.ImportCSVGrocery(file);
+                await _groceryService.ImportCSVGroceryAsync(file);
                 return Ok();
             }
             else
@@ -59,11 +59,11 @@ namespace iTechArt.Api.Controllers
         /// will allow to upload the data from Excel file to db 
         /// </summary>
         [HttpPost(ApiConstants.IMPORTEXCEL)]
-        public async ValueTask<IActionResult> ImportExcelGroceryFile(IFormFile file)
+        public async ValueTask<IActionResult> ImportExcelGroceryFileAsync(IFormFile file)
         {
             if (file != null && file.ContentType.Contains(EXCEL))
             {
-                await _groceryService.ImportExcelGrocery(file);
+                await _groceryService.ImportExcelGroceryAsync(file);
                 return Ok();
             }
             else
@@ -75,11 +75,11 @@ namespace iTechArt.Api.Controllers
         /// will allow to upload the data from XML file to db 
         /// </summary>
         [HttpPost(ApiConstants.IMPORTXML)]
-        public async ValueTask<IActionResult> ImportXMLGroceryFile(IFormFile file)
+        public async ValueTask<IActionResult> ImportXMLGroceryFileAsync(IFormFile file)
         {
             if (file != null && file.ContentType.Contains(XML))
             {
-                await _groceryService.ImportXMLGrocery(file);
+                await _groceryService.ImportXMLGroceryAsync(file);
                 return Ok();
             }
             else
@@ -89,9 +89,9 @@ namespace iTechArt.Api.Controllers
         /// api route which allows to get all info from db and parse it to the following format
         /// </summary>
         [HttpGet("get_all")]
-        public async Task<IActionResult> ExportGroceryData()
+        public async Task<IActionResult> ExportGroceryDataAsync()
         {
-            return Ok(await _groceryService.ExportGrocery());
+            return Ok(await _groceryService.ExportGroceryAsync());
         }
 
 
--- iTechArt.Domain/ParserInterfaces/IAirportParsers.cs
+++ iTechArt.Domain/ParserInterfaces/IAirportParsers.cs
@@ -5,7 +5,7 @@ namespace iTechArt.Domain.ParserInterfaces
     public interface IAirportParsers
     {
         public Task CsvParserAsync(IFormFile file);
-        public Task ExcelParser(IFormFile file);
-        public Task XmlParser(IFormFile file);
+        public Task ExcelParserAsync(IFormFile file);
+        public Task XmlParserAsync(IFormFile file);
     }
 }
