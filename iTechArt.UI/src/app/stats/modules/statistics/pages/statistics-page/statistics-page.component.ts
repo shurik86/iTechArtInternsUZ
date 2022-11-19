f19584fa63df6f599d0a93c18354ff1a9438cc48
Albina <105126389+AlbinaPavlova@users.noreply.github.com>
2022-11-19 14:40:40 +0500

    [UI] downgrade angular version

--- iTechArt.UI/src/app/stats/modules/statistics/pages/statistics-page/statistics-page.component.ts
+++ iTechArt.UI/src/app/stats/modules/statistics/pages/statistics-page/statistics-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { UnitsEnum } from "../../../../../shared/enums/units.enum";
+import { UnitCountDashboardInterface } from "../../../../../shared/interfaces/unit-count-dashboard.interface";
+import 'charts.css';
+
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
   styleUrls: ['./statistics-page.component.scss'],
 })
-export class StatisticsPageComponent {}
+export class StatisticsPageComponent{
+
+
+}
+
+
